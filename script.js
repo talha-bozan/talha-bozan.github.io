b62e3c5eaeac0ec3c89c915e278351d41e841f73
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-11-07 19:40:47 +0300

    SideBar Added.

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".bx-menu");
-console.log(sidebar);
-sidebarBtn.addEventListener("click", ()=>{
-  sidebar.classList.toggle("close");
+const sidebar = document.querySelector('.sidebar');
+const sidebarToggle = document.getElementById('sidebar-toggle');
+const sidebarClose = document.getElementById('sidebar-close');
+let isSidebarOpen = false;
+
+sidebarToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+  isSidebarOpen = !isSidebarOpen;
+
+  if (isSidebarOpen) {
+    sidebarToggle.style.display = 'none';
+    sidebarClose.style.display = 'block'; // Show the close button
+  }
+});
+
+sidebarClose.addEventListener('click', () => {
+  sidebar.classList.remove('open');
+  sidebarToggle.style.display = 'block';
+  sidebarClose.style.display = 'none'; // Hide the close button
+  isSidebarOpen = false;
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50 && !isSidebarOpen) {
+    sidebar.classList.add('open');
+    sidebarToggle.style.display = 'none';
+  } else if (!isSidebarOpen) {
+    sidebar.classList.remove('open');
+    sidebarToggle.style.display = 'block';
+    sidebarClose.style.display = 'none'; // Hide the close button
+  }
+});
+
+window.addEventListener('scroll', () => {
+  if (isSidebarOpen && window.scrollY === 0) {
+    sidebar.classList.remove('open');
+    sidebarToggle.style.display = 'block';
+    sidebarClose.style.display = 'none'; // Hide the close button
+    isSidebarOpen = false;
+  }
 });
 
 const filmLinks = document.querySelectorAll('.film-link');
